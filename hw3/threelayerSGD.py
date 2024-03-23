ea41b3b65b6a579704830b4e4967f9d85024f960
kisuk010203 <chriskisuk@gmail.com>
2024-03-23 19:27:11 +0900

    HW3 Programming Done

--- hw3/threelayerSGD.py
+++ hw3/threelayerSGD.py
@@ -11,19 +11,25 @@ K = 1000
 B = 128
 N = 512
 
-def f_true(x) :
-    return (x-2) * np.cos(x*4)
+
+def f_true(x):
+    return (x - 2) * np.cos(x * 4)
+
 
 torch.manual_seed(0)
-X_train = torch.normal(0.0, 1.0, (N,))
-y_train = f_true(X_train)
-X_val = torch.normal(0.0, 1.0, (N//5,))
+X_train = torch.normal(0.0, 1.0, (N,)) # N unit Gaussian RVs
+y_train = f_true(X_train) + torch.normal(0, 0.5, X_train.shape)
+X_val = torch.normal(0.0, 1.0, (N // 5,))
 y_val = f_true(X_val)
 
-train_dataloader = DataLoader(TensorDataset(X_train.unsqueeze(1), y_train.unsqueeze(1)), batch_size=B)
-test_dataloader = DataLoader(TensorDataset(X_val.unsqueeze(1), y_val.unsqueeze(1)), batch_size=B)
+train_dataloader = DataLoader(
+    TensorDataset(X_train.unsqueeze(1), y_train.unsqueeze(1)), batch_size=B
+)
+test_dataloader = DataLoader(
+    TensorDataset(X_val.unsqueeze(1), y_val.unsqueeze(1)), batch_size=B
+)
 
-'''
+"""
 unsqueeze(1) reshapes the data into dimension [N,1],
 where is 1 the dimension of an data point.
 
@@ -33,27 +39,55 @@ In practice, however, the batchsize for the training dataloader
 is usually chosen to be as large as possible while not exceeding
 the memory size of the GPU. In such cases, it is not possible to
 use a larger batchsize for the test dataloader.
-'''
+"""
+
 
 class MLP(nn.Module):
     def __init__(self):
-        pass
-    def forward(self, x):
-        pass
-        
-        
-        
+        super().__init__()
+        self.linear1 = nn.Linear(1, 64)
+        self.linear2 = nn.Linear(64, 64)
+        self.linear3 = nn.Linear(64, 1)
 
+    def forward(self, x):
+        x = F.sigmoid(self.linear1(x))
+        x = F.sigmoid(self.linear2(x))
+        x = self.linear3(x)
+        return x
+
+model = MLP()
+model.linear1.weight.data = torch.normal(0, 1, model.linear1.weight.shape)
+model.linear2.weight.data = torch.normal(0, 1, model.linear2.weight.shape)
+model.linear3.weight.data = torch.normal(0, 1, model.linear3.weight.shape)
+model.linear1.bias.data = torch.full(model.linear1.bias.shape, 0.03)
+model.linear2.bias.data = torch.full(model.linear2.bias.shape, 0.03)
+model.linear3.bias.data = torch.full(model.linear3.bias.shape, 0.03)
+optimizer = optim.SGD(model.parameters(), lr=alpha)
+
+for epoch in range(K):
+    for X_batch, y_batch in train_dataloader:
+        optimizer.zero_grad()
+        loss = nn.MSELoss()(model(X_batch), y_batch) / 2
+        loss.backward()
+        optimizer.step()
+
+# print("Parameters of MLP model")
+# param_num = 0
+# for param in model.parameters():
+#     print(param.shape)
+#     param_num += param.numel()
+# print("Total number of parameters: ", param_num)
 
 with torch.no_grad():
-    xx = torch.linspace(-2,2,1024).unsqueeze(1)
-    plt.plot(X_train,y_train,'rx',label='Data points')
-    plt.plot(xx,f_true(xx),'r',label='True Fn')
-    plt.plot(xx, model(xx),label='Learned Fn')
+    xx = torch.linspace(-2, 2, 1024).unsqueeze(1)
+    plt.plot(X_train, y_train, "rx", label="Data points")
+    plt.plot(xx, f_true(xx), "r", label="True Fn")
+    plt.plot(xx, model(xx), label="Learned Fn")
 plt.legend()
-plt.show()
+plt.title("MLP with Gaussian Error in Train Dataset")
+plt.savefig("Problem 2_2.pdf")
 
-'''
+"""
 When plotting torch tensors, you want to work with the
 torch.no_grad() context manager.
 
@@ -82,4 +116,4 @@ genuine copy of the detached tensor, and one can freely manipulate and change it
 This discussion will likely not make sense to most students at this point of the course.
 We will revisit this issue after we cover backpropagation.
 
-'''
+"""
